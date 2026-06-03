35736edd5f49ce28d258d83703b2c7bfb470778e
Aelig1 <16878126+Aelig1@users.noreply.github.com>
2026-06-03 21:28:40 +0300

    Clean up mouse capture on panic

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use crossterm::{
     event::{DisableMouseCapture, EnableMouseCapture},
     execute,
 };
-use std::io::stdout;
+use std::{io::stdout, panic};
 
 use crate::app::App;
 
@@ -11,17 +11,39 @@ pub mod event;
 pub mod pond;
 pub mod ui;
 
+fn init() -> ratatui::DefaultTerminal {
+    let terminal = ratatui::init();
+
+    let original_hook = panic::take_hook();
+    panic::set_hook(Box::new(move |info| {
+        disable_mouse_capture();
+        original_hook(info);
+    }));
+    enable_mouse_capture();
+
+    terminal
+}
+
+fn restore() {
+    disable_mouse_capture();
+    ratatui::restore();
+}
+
+fn enable_mouse_capture() {
+    execute!(stdout(), EnableMouseCapture).expect("failed to enable mouse capture");
+}
+
+fn disable_mouse_capture() {
+    let _ = execute!(stdout(), DisableMouseCapture);
+}
+
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
 
-    let terminal = ratatui::init();
-    execute!(stdout(), EnableMouseCapture)?;
-
+    let terminal = init();
     let result = App::new().run(terminal).await;
-
-    execute!(stdout(), DisableMouseCapture)?;
-    ratatui::restore();
+    restore();
 
     result
 }
